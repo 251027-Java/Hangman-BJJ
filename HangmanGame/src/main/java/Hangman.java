5fbafeaae61bdb0cc98087c3a8d2fd3225055142
Code Classroom <benjamin.underdahl@proton.me>
2025-10-29 14:46:45 -0500

    partially completed hangman game

--- HangmanGame/src/main/java/Hangman.java
+++ HangmanGame/src/main/java/Hangman.java
@@ -4,13 +4,22 @@ public class Hangman {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String word;
-        String guess;
+        char[] guessWord;
+        char guess;
         int guessCount = 5;
         System.out.print("Enter a word you want to be guessed: ");
         word = scanner.nextLine();
-        for (int i = 0; i < guessCount; i++) {
+        guessWord = new char[word.length()];
+        while(true) {
             System.out.print("Enter guess: ");
-            guess = scanner.nextLine();
+            guess = scanner.next().charAt(0);
+            for (int j = 0; j < word.length(); j++) {
+                if(guess == word.charAt(j)) {
+                    guessWord[j] = guess;
+                }
+
+            }
+            System.out.println(guessWord);
         }
 
     }
