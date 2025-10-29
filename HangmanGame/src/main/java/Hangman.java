689f876d46f8e59ec7ef1340011084759de894ba
Code Classroom <benjamin.underdahl@proton.me>
2025-10-29 15:16:52 -0500

    added underscores

--- HangmanGame/src/main/java/Hangman.java
+++ HangmanGame/src/main/java/Hangman.java
@@ -3,35 +3,36 @@ import java.util.Scanner;
 public class Hangman {
 
     public static void main(String[] args) {
+        int count = 0;
         Scanner scanner = new Scanner(System.in);
         String word;
         char[] guessWord;
         char guess;
-        int guessCount = 5;
         System.out.print("Enter a word you want to be guessed: ");
         word = scanner.nextLine();
         guessWord = new char[word.length()];
         for (int i = 0; i < word.length(); i++) {
+            System.out.println();
             System.out.print("Enter guess: ");
+
             guess = scanner.next().charAt(0);
             for (int j = 0; j < word.length(); j++) {
                 if(guess == word.charAt(j)) {
                     guessWord[j] = guess;
+                    count++;
                 }
-
+                if (count == word.length()) {
+                    System.out.println("You won");
+                    return;
+                }
             }
-            System.out.println(guessWord);
-        }
-        int count = 0;
-        for (int i = 0; i < word.length(); i++) {
-            if (guessWord[i] == word.charAt(i)) {
-                count++;
+            for (int j = 0; j < word.length(); j++) {
+                if (guessWord[j] == 0) {
+                    System.out.print("_ ");
+                } else {
+                    System.out.print(guessWord[j] + " ");
+                }
             }
-        }
-        if (count == word.length()) {
-            System.out.println("You Won");
-        } else {
-            System.out.println("You Lost");
         }
 
     }
